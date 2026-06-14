9c432f45321dbe7424fe177cbcb4122c403af9e0
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2026-06-14 16:50:52 +0530

    different users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,32 +2,58 @@ import { useState, useEffect, useRef } from "react";
 function App() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
+  const URL = import.meta.env.VITE_API_WURL;
 
   const socketRef = useRef(null);
   useEffect(() => {
-    socketRef.current = new WebSocket("ws://localhost:8000/ws/1");
+    socketRef.current = new WebSocket(`${URL}/ws/1/saurabh`);
     socketRef.current.onmessage = (event) => {
       console.log("message came");
-      setMessages((prev)=>([...prev, event.data]))
+      console.log(messages);
+      setMessages((prev) => [...prev, event.data]);
     };
-
   }, []);
+
+  useEffect(() => {
+    console.log(messages);
+  }, [messages]);
 
   const sendmessage = () => {
     socketRef.current.send(input);
     setInput("");
-    setMessages((prev) => ([...prev, input]));
+    // setMessages((prev) => [...prev, input]);
   };
   return (
-    <div className="bg-violet-950 w-full min-h-screen">
-      
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <div onClick={sendmessage}>submit</div>
-      <div>
-        {messages.map((message, index)=>(
-          <div key={index}>{message}</div>
-          
-        ))}
+    <div className="bg-sky-900 w-full min-h-screen flex items-center justify-center flex-col gap-2 ">
+      <div className="text-4xl text-sky-100 font-bold fixed top-0 bg-sky-950 w-full p-2 flex items-center justify-center">
+        chat app
+      </div>
+      <div className="bg-sky-950 p-2 rounded-md w-1/2 overflow-y-auto h-125 scroll-">
+        {messages.map((message, index) => {
+          const msg = JSON.parse(message);
+          return (
+            <div key={index} className="flex flex-row gap-2">
+              <div>{msg.username}:</div>
+              <div>{msg.data}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex flex-row gap-2 fixed bottom-0 w-full">
+        <div className="h-fit w-11/12">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="border-2 border-sky-950 rounded-md p-2 font-semibold text-sky-200 focus:outline-0 focus:border-b-2 focus:border-b-sky-400 caret-sky-500 w-full"
+            spellCheck="false"
+          />
+        </div>
+        <div
+          onClick={sendmessage}
+          className="cursor-pointer border border-sky-950 rounded-md p-2 font-semibold bg-sky-400 w-1/12"
+        >
+          submit
+        </div>
       </div>
     </div>
   );
--- server/main.py
+++ server/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 rooms = {}
 
@@ -8,14 +27,14 @@ rooms = {}
 def text_app():
     return {"message": "ok"}
 
-@app.websocket("/ws/{room_id}")
-async def websocket_endpoint(websocket: WebSocket, room_id: int):
+@app.websocket("/ws/{room_id}/{username}")
+async def websocket_endpoint(websocket: WebSocket, room_id: int, username: str):
     await websocket.accept()
 
     if room_id not in rooms:
         rooms[room_id] = []
 
-    rooms[room_id].append(websocket)
+    rooms[room_id].append({"socket":websocket, "username":username})
 
     print(rooms)
 
@@ -24,13 +43,21 @@ async def websocket_endpoint(websocket: WebSocket, room_id: int):
             data = await websocket.receive_text()
 
             for client in rooms[room_id]:
-                if client != websocket:
-                    await client.send_text(data)
+                if client["socket"] != websocket:
+                    await client["socket"].send_json({
+                        "username":username,
+                        "data":data})
 
+    # except WebSocketDisconnect:
+    #     rooms[room_id].remove(websocket)
+
+    #     if len(rooms[room_id]) == 0:
+    #         del rooms[room_id]
+
+    #     print(rooms)
     except WebSocketDisconnect:
-        rooms[room_id].remove(websocket)
-
-        if len(rooms[room_id]) == 0:
-            del rooms[room_id]
-
-        print(rooms)
+        rooms[room_id] = [
+            client
+            for client in rooms[room_id]
+            if client["socket"] != websocket
+        ]
